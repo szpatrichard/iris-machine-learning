9b740f63d300adbc7e41cf27826ced2f90f3ce7e
Richárd Szilágyi <patrik.szilagyi@mycit.ie>
2022-10-26 08:33:41 +0100

    Add python script to check versions of libraries

---
+++ check_versions.py
@@ -0,0 +1,14 @@
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+print(f"Python v{sys.version}\n"
+      f"SciPy v{scipy.__version__}\n"
+      f"NumPy v{numpy.__version__}\n"
+      f"MatPlotLib v{matplotlib.__version__}\n"
+      f"Pandas v{pandas.__version__}\n"
+      f"SciKit Learn v{sklearn.__version__}\n"
+)
